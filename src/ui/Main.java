93ed6f316ef51b83c7cec90cdac5309b828f73b9
Santiago <santiago.rodas1@correo.icesi.edu.co>
2020-10-28 15:44:12 -0500

    Basic structure with the Java FX

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,76 @@
+/*
+ * ALGORITMOS Y ESTRUCTURAS DE DATOS
+ * TAREA INTEGRADORA 2
+ * MARTINEZ - DIAZ - RODAS
+ */
+
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Main extends Application {
+	
+	//------------------------------------------------------------------------------------
+	
+	// RELATION'S WITH ANOTHER CLASS
+	
+	private PrincipalController controller;
+	
+	//------------------------------------------------------------------------------------
+	
+	// CONTRUCTOR METHOD OF THE MAIN CLASS
+	
+	public Main() {
+		
+		controller = new PrincipalController();
+		
 	}
+	
+	//------------------------------------------------------------------------------------
+	
+	// METHOD START OF THE CLASS MAIN
+	
+	@Override
+	public void start(Stage primaryStage) {
+
+		try {
+			
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PrincipalPanel.fxml"));
+
+			fxmlLoader.setController(controller);
+			
+			Parent root = fxmlLoader.load();
+			
+			Scene scene = new Scene(root);
+			
+			primaryStage.setScene(scene);
+			
+			primaryStage.setTitle("People database");
+			
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+			
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	// MAIN METHOD (PRINCIPAL METHOD)
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+	
+	//------------------------------------------------------------------------------------
 
 }
