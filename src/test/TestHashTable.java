115fedb9b074c716dc0653c4886808593aa0014a
Alejandra Diaz <alejandradp19@gmail.com>
2020-11-01 16:00:43 -0500

    Added class TestAVL (again). Checked some problems in TestHashTable

--- src/test/TestHashTable.java
+++ src/test/TestHashTable.java
@@ -7,70 +7,71 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+
 import collections.HashTable;
 
 class TestHashTable<K,V> {
-	
 	//------------------------------------------------------------------------------------
 	
-	// Relation with the class Hash Table
+		// Relation with the class Hash Table
 
-	HashTable <K,V> hashTable;
-	
-	//------------------------------------------------------------------------------------
-	
-	// Setup1
-
-	void setup1() {
-
-		hashTable = new HashTable<K, V>();
-
-	}
-	
-	//------------------------------------------------------------------------------------
-
-	// We check a case where we have the same data
-	
-	@Test
-	void testInsert1() {
-
-		setup1();
+		HashTable <K,V> hashTable;
 		
-		for(int i = 0 ; i < 3 ; i ++) {
-			
-			@SuppressWarnings("unchecked")
-			K key = (K) new Integer(10);
-			
-			@SuppressWarnings("unchecked")
-			V value = (V) new Integer(100);
-			
-			hashTable.insert(key, value);
-			
+		//------------------------------------------------------------------------------------
+		
+		// Setup1
+
+		void setup1() {
+
+			hashTable = new HashTable<K, V>();
+
 		}
 		
-		assertEquals(hashTable.size(), 3);
+		//------------------------------------------------------------------------------------
 
-	}
-	
-	//------------------------------------------------------------------------------------
+		// We check a case where we have the same data
+		
+		@Test
+		void testInsert1() {
 
-	@Test
-	void testInsert2() {
+			setup1();
+			
+			for(int i = 0 ; i < 3 ; i ++) {
+				
+				@SuppressWarnings("unchecked")
+				K key = (K) new Integer(10);
+				
+				@SuppressWarnings("unchecked")
+				V value = (V) new Integer(100);
+				
+				hashTable.insert(key, value);
+				
+			}
+			
+			assertEquals(hashTable.size(), 3);
 
-		setup1();
+		}
+		
+		//------------------------------------------------------------------------------------
 
-	}
-	
-	//------------------------------------------------------------------------------------
+		@Test
+		void testInsert2() {
 
-	@Test
-	void testInsert3() {
+			setup1();
 
-		setup1();
+		}
+		
+		//------------------------------------------------------------------------------------
 
-	}
-	
-	//------------------------------------------------------------------------------------
-	
+		@Test
+		void testInsert3() {
+
+			setup1();
+
+		}
+		
+		//------------------------------------------------------------------------------------
+		
 }
