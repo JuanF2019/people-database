190a53fc0697fc0f4cc8b2ef8a095a2c224c1f2e
Santiago <santiago.rodas1@correo.icesi.edu.co>
2020-11-06 20:04:38 -0500

    New test model

---
+++ test/model/TestRandomFieldsGenerator.java
@@ -0,0 +1,24 @@
+/*
+ * UNIVERSIDAD ICESI
+ * TAREA INTEGRADORA 2 - ESTRUCTURAS DE DATOS
+ * RODAS / DIAZ / MARTINEZ
+ */
+
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRandomFieldsGenerator {
+	
+	//------------------------------------------------------------------------------------
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	//------------------------------------------------------------------------------------
+
+}
